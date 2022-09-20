a454e7dd9e199f36120b9a0e7a0c75614d8aae42
sharathpc <sharathchandra0033@gmail.com>
2022-09-20 23:14:02 +0530

    Experience section dynamic data

--- components/ExperienceSection.tsx
+++ components/ExperienceSection.tsx
@@ -1,123 +1,73 @@
+import React from "react"
+
 const ExperienceSection = () => {
+    const data = {
+        sectionHeading: 'My Resume',
+        subSections: [{
+            heading: 'Work Experience',
+            iconClassName: 'icon-suitcase',
+            list: [{
+                mainText: 'Senior Developer',
+                subText: 'Company Name - 2016 - Current',
+                description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.'
+            }, {
+                mainText: 'Senior Developer',
+                subText: 'Company Name - 2016 - Current',
+                description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.'
+            }]
+        }, {
+            heading: 'Education',
+            iconClassName: 'icon-graduation-cap',
+            list: [{
+                mainText: 'Masters Degree',
+                subText: 'University Name - 2007 - 2009',
+                description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.'
+            }]
+        }]
+    }
+
     return (
         <div id="fh5co-resume" className="fh5co-bg-color">
             <div className="container">
                 <div className="row animate-box">
                     <div className="col-md-8 offset-md-2 text-center fh5co-heading">
-                        <h2>My Resume</h2>
+                        <h2>{data.sectionHeading}</h2>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-12 offset-md-0">
                         <ul className="timeline">
-                            <li className="timeline-heading text-center animate-box">
-                                <div>
-                                    <h3>Work Experience</h3>
-                                </div>
-                            </li>
-                            <li className="animate-box timeline-unverted">
-                                <div className="timeline-badge"><i className="icon-suitcase"></i></div>
-                                <div className="timeline-panel">
-                                    <div className="timeline-heading">
-                                        <h3 className="timeline-title">Senior Developer</h3>
-                                        <span className="company">Company Name - 2016 - Current</span>
+                            {data.subSections.map((subSection, subIndex) =>
+                                <React.Fragment key={subIndex}>
+                                    <div className="timeline-heading text-center animate-box">
+                                        <div>
+                                            <h3>{subSection.heading}</h3>
+                                        </div>
                                     </div>
-                                    <div className="timeline-body">
-                                        <p>Far far away, behind the word mountains, far from the countries Vokalia and
-                                            Consonantia, there live the blind texts.</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li className="timeline-inverted animate-box">
-                                <div className="timeline-badge"><i className="icon-suitcase"></i></div>
-                                <div className="timeline-panel">
-                                    <div className="timeline-heading">
-                                        <h3 className="timeline-title">Junior Developer</h3>
-                                        <span className="company">Company Name - 2013 - 2015</span>
-                                    </div>
-                                    <div className="timeline-body">
-                                        <p>Far far away, behind the word mountains, they live in Bookmarksgrove right at
-                                            the coast of the Semantics, a large language ocean.</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li className="animate-box timeline-unverted">
-                                <div className="timeline-badge"><i className="icon-suitcase"></i></div>
-                                <div className="timeline-panel">
-                                    <div className="timeline-heading">
-                                        <h3 className="timeline-title">UI/UX Designer</h3>
-                                        <span className="company">Company Name - 2010 - 2012</span>
-                                    </div>
-                                    <div className="timeline-body">
-                                        <p>Far far away, behind the word mountains, far from the countries Vokalia and
-                                            Consonantia, there live the blind texts.</p>
-                                    </div>
-                                </div>
-                            </li>
-
-                            <br />
-                            <li className="timeline-heading text-center animate-box">
-                                <div>
-                                    <h3>Education</h3>
-                                </div>
-                            </li>
-                            <li className="timeline-inverted animate-box">
-                                <div className="timeline-badge"><i className="icon-graduation-cap"></i></div>
-                                <div className="timeline-panel">
-                                    <div className="timeline-heading">
-                                        <h3 className="timeline-title">Masters Degree</h3>
-                                        <span className="company">University Name - 2007 - 2009</span>
-                                    </div>
-                                    <div className="timeline-body">
-                                        <p>Far far away, behind the word mountains, they live in Bookmarksgrove right at
-                                            the coast of the Semantics, a large language ocean.</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li className="animate-box timeline-unverted">
-                                <div className="timeline-badge"><i className="icon-graduation-cap"></i></div>
-                                <div className="timeline-panel">
-                                    <div className="timeline-heading">
-                                        <h3 className="timeline-title">Bachelors Degree</h3>
-                                        <span className="company">University Name - 2002 - 2006</span>
-                                    </div>
-                                    <div className="timeline-body">
-                                        <p>Far far away, behind the word mountains, far from the countries Vokalia and
-                                            Consonantia, there live the blind texts.</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li className="timeline-inverted animate-box">
-                                <div className="timeline-badge"><i className="icon-graduation-cap"></i></div>
-                                <div className="timeline-panel">
-                                    <div className="timeline-heading">
-                                        <h3 className="timeline-title">Diploma Course</h3>
-                                        <span className="company">College Name - 1999 - 2001</span>
-                                    </div>
-                                    <div className="timeline-body">
-                                        <p>Far far away, behind the word mountains, they live in Bookmarksgrove right at
-                                            the coast of the Semantics, a large language ocean.</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li className="animate-box timeline-unverted">
-                                <div className="timeline-badge"><i className="icon-graduation-cap"></i></div>
-                                <div className="timeline-panel">
-                                    <div className="timeline-heading">
-                                        <h3 className="timeline-title">Graduation</h3>
-                                        <span className="company">College Name - 1994 - 1998</span>
-                                    </div>
-                                    <div className="timeline-body">
-                                        <p>Far far away, behind the word mountains, far from the countries Vokalia and
-                                            Consonantia, there live the blind texts.</p>
-                                    </div>
-                                </div>
-                            </li>
+                                    {subSection.list.map((item, itemIndex) =>
+                                        <li className="animate-box timeline-item"
+                                            key={`${subIndex}-${itemIndex}`}>
+                                            <div className="timeline-badge">
+                                                <i className={subSection.iconClassName}></i>
+                                            </div>
+                                            <div className="timeline-panel">
+                                                <div className="timeline-heading">
+                                                    <h3 className="timeline-title">{item.mainText}</h3>
+                                                    <span className="company">{item.subText}</span>
+                                                </div>
+                                                <div className="timeline-body">
+                                                    <p>{item.description}</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    )}
+                                </React.Fragment>
+                            )}
                         </ul>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
