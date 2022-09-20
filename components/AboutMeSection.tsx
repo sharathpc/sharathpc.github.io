6a279482be6fd52b6199a35e9103f3d4866046c6
sharathpc <sharathchandra0033@gmail.com>
2022-09-20 22:00:14 +0530

    About Section Data conversion

--- components/AboutMeSection.tsx
+++ components/AboutMeSection.tsx
@@ -1,45 +1,45 @@
 const AboutMeSection = () => {
+    const data = {
+        sectionHeading: 'About Me',
+        helloText: 'Hello There!',
+        descriptionBlocks: [
+            'There live the blind texts far from the countries Vokalia and Consonantia, there live the blind texts.Separated they live in bookmarksgrove there live the blind texts far from the countries.',
+            'Far far away, behind the word mountains, Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci there live the blind texts from the countries Vokalia and Consonantia, there live the blind texts. Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.'
+        ],
+        fieldsList: [{
+            label: 'First Name',
+            value: 'Sharath Chandra',
+        }, {
+            label: 'Last Name',
+            value: 'Pyarasani',
+        }, {
+            label: 'Email',
+            value: 'sharathchandra0033@gmail.com',
+        }]
+    }
+
     return (
         <div id="fh5co-about" className="animate-box">
             <div className="container">
                 <div className="row">
                     <div className="col-md-8 offset-md-2 text-center fh5co-heading">
-                        <h2>About Me</h2>
+                        <h2>{data.sectionHeading}</h2>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-4">
                         <ul className="info">
-                            <li><span className="first-block">Full Name:</span><span className="second-block">Louie Jie
-                                Mahusay</span></li>
-                            <li><span className="first-block">Phone:</span><span className="second-block">+ 1235 2355 98</span>
-                            </li>
-                            <li><span className="first-block">Email:</span><span
-                                className="second-block">info@yoursite.com</span></li>
-                            <li><span className="first-block">Website:</span><span
-                                className="second-block">www.yoursite.com</span></li>
-                            <li><span className="first-block">Address:</span><span className="second-block">198 West 21th
-                                Street, Suite 721 New York NY 10016</span></li>
+                            {data.fieldsList.map((field, fIndex) =>
+                                <li key={fIndex}>
+                                    <span className="first-block">{field.label}:</span>
+                                    <span className="second-block">{field.value}</span>
+                                </li>
+                            )}
                         </ul>
                     </div>
                     <div className="col-md-8">
-                        <h2>Hello There!</h2>
-                        <p>There live the blind texts far from the countries Vokalia and Consonantia, there live the
-                            blind texts. Separated they live in bookmarksgrove there live the blind texts far from the
-                            countries.</p>
-                        <p>Far far away, behind the word mountains, Quos quia provident consequuntur culpa facere
-                            ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus
-                            itaque adipisci there live the blind texts from the countries Vokalia and Consonantia, there
-                            live the blind texts. Quos quia provident consequuntur culpa facere ratione maxime commodi
-                            voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.</p>
-                        <div>
-                            <ul className="fh5co-social-icons">
-                                <li><a href="#"><i className="icon-twitter2"></i></a></li>
-                                <li><a href="#"><i className="icon-facebook3"></i></a></li>
-                                <li><a href="#"><i className="icon-linkedin2"></i></a></li>
-                                <li><a href="#"><i className="icon-dribbble2"></i></a></li>
-                            </ul>
-                        </div>
+                        <h2>{data.helloText}</h2>
+                        {data.descriptionBlocks.map((desc, dIndex) => <p key={dIndex}>{desc}</p>)}
                     </div>
                 </div>
             </div>
