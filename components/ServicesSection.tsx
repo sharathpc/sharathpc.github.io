65c151b88cb36a5e68e2670ce5889b4b03a3f321
sharathpc <sharathchandra0033@gmail.com>
2022-09-20 23:22:48 +0530

    Services Section dynamic data

--- components/ServicesSection.tsx
+++ components/ServicesSection.tsx
@@ -1,90 +1,44 @@
 const ServicesSection = () => {
+    const data = {
+        sectionHeading: 'My Services',
+        list: [{
+            iconClassName: 'icon-paintbrush',
+            mainText: 'Web Design',
+            subText: 'Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.'
+        }, {
+            iconClassName: 'icon-genius',
+            mainText: 'Web Development',
+            subText: 'Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.'
+        }, {
+            iconClassName: 'icon-mobile',
+            mainText: 'Mobile Development',
+            subText: 'Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.'
+        }]
+    }
+
     return (
         <div id="fh5co-features" className="animate-box">
             <div className="container">
                 <div className="services-padding">
                     <div className="row">
                         <div className="col-md-8 offset-md-2 text-center fh5co-heading">
-                            <h2>My Services</h2>
+                            <h2>{data.sectionHeading}</h2>
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-md-4 text-center">
-                            <div className="feature-left">
-                                <span className="icon">
-                                    <i className="icon-paintbrush"></i>
-                                </span>
-                                <div className="feature-copy">
-                                    <h3>Web Design</h3>
-                                    <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+                        {data.list.map((item, itemIndex) =>
+                            <div className="col-md-4 text-center" key={itemIndex}>
+                                <div className="feature-left">
+                                    <span className="icon">
+                                        <i className={item.iconClassName}></i>
+                                    </span>
+                                    <div className="feature-copy">
+                                        <h3>{item.mainText}</h3>
+                                        <p>{item.subText}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-
-                        <div className="col-md-4 text-center">
-                            <div className="feature-left">
-                                <span className="icon">
-                                    <i className="icon-briefcase"></i>
-                                </span>
-                                <div className="feature-copy">
-                                    <h3>Branding</h3>
-                                    <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="col-md-4 text-center">
-                            <div className="feature-left">
-                                <span className="icon">
-                                    <i className="icon-search"></i>
-                                </span>
-                                <div className="feature-copy">
-                                    <h3>Analytics</h3>
-                                    <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div className="row">
-                        <div className="col-md-4 text-center">
-
-                            <div className="feature-left">
-                                <span className="icon">
-                                    <i className="icon-bargraph"></i>
-                                </span>
-                                <div className="feature-copy">
-                                    <h3>Web Developent</h3>
-                                    <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
-                                </div>
-                            </div>
-
-                        </div>
-
-                        <div className="col-md-4 text-center">
-                            <div className="feature-left">
-                                <span className="icon">
-                                    <i className="icon-genius"></i>
-                                </span>
-                                <div className="feature-copy">
-                                    <h3>Web Marketing</h3>
-                                    <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="col-md-4 text-center">
-                            <div className="feature-left">
-                                <span className="icon">
-                                    <i className="icon-chat"></i>
-                                </span>
-                                <div className="feature-copy">
-                                    <h3>Support</h3>
-                                    <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
-                                </div>
-                            </div>
-                        </div>
+                        )}
                     </div>
                 </div>
             </div>
